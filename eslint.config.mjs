120cdbd478aabac3ab55f9a573024711995c7f81
Hasib Muharam <hasibmuha@gmail.com>
2025-06-28 10:07:50 +0700

    chore(ci): update GitHub Actions for CI & npm publish

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    ignores: ["dist", "dist/**"],
+    ignores: ["dist", "dist/**", "eslint.config.mjs"],
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
