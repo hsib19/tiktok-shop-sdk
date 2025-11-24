ac568f4f959277864d28744270c8c883e423c6da
Hasib Muharam <hasibmuha@gmail.com>
2025-11-25 02:41:40 +0700

    chore: update config nextjs, package.json docs project

--- apps/landing/next.config.ts
+++ apps/landing/next.config.ts
@@ -2,11 +2,15 @@ import type { NextConfig } from 'next';
 import i18nConfig from './next-i18next.config';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   i18n: i18nConfig.i18n,
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
   },
 };
 
